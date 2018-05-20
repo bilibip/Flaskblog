2412f548d57df1632d8cd945000f849801a3d602
The bilibip <bilbip@free.fr>
2018-05-21 01:06:02 +0200

    Add Register and Login

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template 
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'bf1f507a883457ccf1081f6506f74ab5'
 
 posts = [
     {
@@ -17,12 +19,32 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'test':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash('Login unsucessful! Please check your Email and password', 'danger')
+            return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
