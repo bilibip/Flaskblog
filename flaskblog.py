71e16b0a13e7fa055d316910221163a980cb8047
The bilibip <bilbip@free.fr>
2018-05-20 23:07:50 +0200

    Ajout Template Jinja + CSS

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,28 @@
-from flask import Flask 
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': "Daniel RIVIERE",
+        'title': "My second flask app",
+        'content': "First post content"
+    },
+    {
+        'author': "Test User",
+        'title': "Second post message",
+        'content': "Second post content"
+    }
+]
+
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "Hello World!"
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
-    app.run(localhost="0.0.0.0",debug=True)
+    app.run(debug=True)
