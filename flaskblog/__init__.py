f8451f87379319fe5f43a35fa6577bc6af1ef0b8
The bilibip <bilbip@free.fr>
2018-05-21 19:23:46 +0200

    Add hash password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bf1f507a883457ccf1081f6506f74ab5'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return "User('{}','{}','{}').format(self.username, self.email, self.image_file)"
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,4 +21,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return "Post('{}', '{}').format(self.title,self.date_posted)"
