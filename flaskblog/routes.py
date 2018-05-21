e62727881d4c2ba0ff1e6c603e5bfa34af795e55
The bilibip <bilbip@free.fr>
2018-05-21 19:25:54 +0200

    Hashed pasword

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -33,7 +33,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Your account has been created. You can login now!', 'success')
+        flash('Your account has been created. You can login now', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
